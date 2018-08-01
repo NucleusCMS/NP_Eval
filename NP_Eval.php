a9fb4f85f61431761d35797770da21d1223c9b7f
yamamoto <yamamoto@kyms.jp>
2018-08-01 22:28:33 +0900

    Fix for PHP 7.2

--- NP_Eval.php
+++ NP_Eval.php
@@ -3,15 +3,15 @@ class NP_Eval extends NucleusPlugin
 {
 	function getName()              {return 'NP_Eval'; }
 	function getAuthor()            {return 'yama'; }
-	function getVersion()           {return '0.11'; }
-	function getURL()               {return '';}
+	function getVersion()           {return '0.2'; }
+	function getURL()               {return 'https://github.com/NucleusCMS/NP_Eval';}
 	function getDescription()       {return '';}
 	function supportsFeature($w)    {return ($w=='SqlTablePrefix')?1:0; }
 	function getMinNucleusVersion() {return 330; }
 
-	function doSkinVar()     {$this->output($params = func_get_args());}
-	function doTemplateVar() {$this->output($params = func_get_args());}
-	function doItemVar()     {$this->output($params = func_get_args());}
+	function doSkinVar($skinType='') {$this->output($params = func_get_args());}
+	function doTemplateVar(&$item) {$this->output($params = func_get_args());}
+	function doItemVar(&$item)     {$this->output($params = func_get_args());}
 	
 	function output($params)
 	{
